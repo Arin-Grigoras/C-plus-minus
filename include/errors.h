b459c30260ce01b23314dab10963876610ae44c6
Arin <aringrigoras@gmail.com>
2021-04-20 17:33:58 +0300

    more error codes

--- include/errors.h
+++ include/errors.h
@@ -5,12 +5,22 @@
 
 //errors & warningsthat can happen in the program
 
-enum _Errors{
-    SyntaxError = 0,
-    OverFlowError = 1,
-    WhiteSpaceWarning = 2,
-};
-typedef enum _Errors Errors;
+
+//ERRORS
+#define SyntaxError 0
+#define OverFlowError 1
+#define ArithmeticError 2
+#define FloatingPointError 3
+#define IncludeError 4
+#define MemoryOverload 5
+#define RunTimeError 6
+#define ZeroDivisionError 7
+#define FileNotExist 8
+#define TypeError 9
+
+
+//WARNINGS
+#define WhiteSpaceWarning 10
 
 
 void error(const int error_code, char *lex);
--- src/errors.c
+++ src/errors.c
@@ -1,15 +1,47 @@
 #include "../include/C+-.h"
 
 void error(const int error_code, char *lex){
-   if(error_code is SyntaxError){
+    if(error_code is SyntaxError){
        printf("\033[0;31mSyntax error: \033[0;37m no such instruction '%s'\n", lex);
-   }
+    }
 
-   elif(error_code is OverFlowError){
+    elif(error_code is OverFlowError){
        printf("\033[0;31mOverFlow error: \033[0;37m%s\n", lex);
-   }
+    }
 
-   elif(error_code is WhiteSpaceWarning){
-       printf("\033[0;35mWhiteSpaceWarning: %s\n", lex);
-   }
+    elif(error_code is WhiteSpaceWarning){
+       printf("\033[0;35mWhiteSpaceWarning: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is ArithmeticError){
+        printf("033[0;35mArithmetic Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is FloatingPointError){
+        printf("033[0;35mFloating Point Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is IncludeError){
+        printf("033[0;35mInclude Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is MemoryOverload){
+        printf("033[0;35mMemory Overload: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is RunTimeError){
+        printf("033[0;35mInclude Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is ZeroDivisionError){
+        printf("033[0;35mInclude Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is FileNotExist){
+        printf("033[0;35mInclude Error: \033[0;37m%s\n", lex);
+    }
+
+    elif(error_code is TypeError){
+        printf("033[0;35mInclude Error: \033[0;37m%s\n", lex);
+    }
 }
