101bd5df429f5ea0821ca130e27651ee8c229b67
MrGithub2021 <77593158+MrGithub2021@users.noreply.github.com>
2021-04-21 13:10:31 -0400

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,15 @@
+@ECHO off
+
+TITLE The C+- Console
+
+echo ░█████╗░░░░░░░░░░░░░░
+echo ██╔══██╗░░██╗░░░░░░░░
+echo ██║░░╚═╝██████╗█████╗
+echo ██║░░██╗╚═██╔═╝╚════╝
+echo ╚█████╔╝░░╚═╝░░░░░░░░
+echo ░╚════╝░░░░░░░░░░░░░░
+
+echo Welcome to the C+- console.
+echo ===========================================
+echo To contribute, go to https://github.com/koreaneggroll/C-plus-minus
+echo ===========================================
